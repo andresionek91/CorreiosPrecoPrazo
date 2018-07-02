51882280cec1b1915799241a6c571e4a1f83e7cb
Andre Sionek <andre.sionek@olist.com>
2018-07-02 12:13:14 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='CorreiosPrecoPrazo',
     version='1.0',
     py_modules=["api"],
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     install_requires=['xmltodict', 'requests'],
     keywords='correios preços prazos webservice correio api preço prazo',  # Optional
     url='',
