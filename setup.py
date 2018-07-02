80c0d57cbe6a9bdc6fbfd4dc1db8403545295862
Andre Sionek <andre.sionek@olist.com>
2018-07-02 11:32:12 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='correiospy',
+    name='prepra_correios',
     version='1.0',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     install_requires=['xmltodict', 'requests'],
